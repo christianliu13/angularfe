f3d812047685e3ac9f2abde4d89fafbe4432c109
Christian Liu <christianliu13@gmail.com>
2021-06-24 22:19:46 -0700

    form fe

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { FormBuilder, Validators, ValidatorFn } from '@angular/forms';
 import data from '../../assets/questions.json';
 
@@ -13,19 +13,30 @@ export class FormComponent {
     firstName: [null, Validators.required],
     lastName: [null, Validators.required],
   });
-  public questions = data;
+  questions = data;
 
   constructor(private fb: FormBuilder) {}
 
+  
+  saveResponses(data: object){
+    let key = 'formData';
+    const item = JSON.parse(localStorage.getItem(key) || '[]');
+    item.push(data);
+    
+    console.log(item);
+    localStorage.setItem('formData', JSON.stringify(data));
+  }
+
   onSubmit(): void {
+    this.saveResponses(this.questionsForm.value);
     alert('Responses Recorded!');
   }
 
   ngOnInit(){
-    this.questions.forEach(control => this.questionsForm.addControl(control.key, this.fb.control('')));
-    console.log(this.questions)
+    this.questions.forEach(control => this.questionsForm.addControl(control.key, this.fb.control(''))); //get questions data, could be api
   }
 
+//todo add validators
   // private getValidators(formField: FormField): ValidatorFn {
   //   switch(formField.validator) {
   //     case "email":
